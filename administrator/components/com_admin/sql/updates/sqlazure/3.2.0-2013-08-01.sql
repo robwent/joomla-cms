d68c54dc574f65dc30f3e9234e67c3b2c83e6ea5
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2013-08-03 14:58:24 +0300

    gh-96 Schema update to actually install the plugin

--- administrator/components/com_admin/sql/updates/sqlazure/3.2.0-2013-08-01.sql
+++ administrator/components/com_admin/sql/updates/sqlazure/3.2.0-2013-08-01.sql
@@ -1,3 +1,6 @@
 ALTER TABLE [#__users] ADD [otpKey] [nvarchar](max) NOT NULL DEFAULT '';
 
 ALTER TABLE [#__users] ADD [otep] [nvarchar](max) NOT NULL DEFAULT '';
+
+INSERT INTO #__extensions (extension_id, name, type, element, folder, client_id, enabled, access, protected, manifest_cache, params, custom_data, system_data, checked_out, checked_out_time, ordering, state)
+SELECT 448, 'plg_twofactorauth_totp', 'plugin', 'totp', 'twofactorauth', 0, 1, 1, 0, '', '{}', '', '', 0, '1900-01-01 00:00:00', 0, 0;
