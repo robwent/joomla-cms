a9a43d3d57cbdaaced7feb5f70a10c8fbce547f2
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2013-08-10 00:19:26 +0300

    gh-104 Schema changes on update

---
+++ administrator/components/com_admin/sql/updates/sqlazure/3.2.0-2013-08-10.sql
@@ -0,0 +1,2 @@
+INSERT INTO #__extensions (extension_id, name, type, element, folder, client_id, enabled, access, protected, manifest_cache, params, custom_data, system_data, checked_out, checked_out_time, ordering, state)
+SELECT 450, 'plg_system_updatenotification', 'plugin', 'updatenotification', 'SYSTEM', 0, 1, 1, 0, '', '{}', '', '', 0, '1900-01-01 00:00:00', 0, 0;
